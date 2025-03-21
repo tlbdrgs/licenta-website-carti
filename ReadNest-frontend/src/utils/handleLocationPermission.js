b262f05cea3dd0f3fc0f5a0dab510b94f5feaf88
Dragos Talaba <talaba.dragos@gmail.com>
2025-03-21 13:18:23 +0200

    vite frontend

--- ReadNest-frontend/src/utils/handleLocationPermission.js
+++
@@ -1,8 +0,0 @@
-export const checkLocationPermission = () => {
-    return new Promise((resolve, reject) => {
-      navigator.geolocation.getCurrentPosition(
-        (position) => resolve(position.coords),
-        (error) => reject('Location access denied')
-      );
-    });
-  };
